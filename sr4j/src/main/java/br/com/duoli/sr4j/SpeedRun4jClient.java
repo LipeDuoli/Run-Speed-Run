d04b3f8636031a0d43a02c02fe76144354d5f309
LipeDuoli <lipeduoli@gmail.com>
2018-04-24 21:50:22 -0300

    set timeout to retrofit request

--- sr4j/src/main/java/br/com/duoli/sr4j/SpeedRun4jClient.java
+++ sr4j/src/main/java/br/com/duoli/sr4j/SpeedRun4jClient.java
@@ -1,11 +1,14 @@
 package br.com.duoli.sr4j;
 
+import java.util.concurrent.TimeUnit;
+
 import br.com.duoli.sr4j.fluent.game.GameSearch;
 import br.com.duoli.sr4j.fluent.leaderboards.LeaderBoardsSearch;
 import br.com.duoli.sr4j.fluent.runs.RunSearch;
 import br.com.duoli.sr4j.services.GameService;
 import br.com.duoli.sr4j.services.LeaderboardsService;
 import br.com.duoli.sr4j.services.RunService;
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -13,8 +16,14 @@ public class SpeedRun4jClient {
 
     private static final String BASE_URL = "https://www.speedrun.com/api/v1/";
 
+    private static OkHttpClient okHttpClient = new OkHttpClient.Builder()
+            .readTimeout(60, TimeUnit.SECONDS)
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .build();
+
     private static Retrofit.Builder builder = new Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create());
 
     private static <S> S createService(Class<S> clazz) {
